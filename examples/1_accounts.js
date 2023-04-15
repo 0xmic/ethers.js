8df91b520d656a8198fdeb22453fb8e348d8c5c4
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-04-15 23:51:05 +0000

    Update examples

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,12 +1,14 @@
 const { ethers } = require("ethers");
-const rpcURL = 'https://cloudflare-eth.com/';
+const rpcURL = "https://cloudflare-eth.com/";
 const provider = new ethers.providers.JsonRpcProvider(rpcURL);
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e';
+const address = "0xmic.eth";
 
 const main = async () => {
-    const balance = await provider.getBalance(address);
-    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`);
+  const balance = await provider.getBalance(address);
+  console.log(
+    `\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`
+  );
 };
 
 main();
